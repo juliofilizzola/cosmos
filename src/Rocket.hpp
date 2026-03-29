79d05fe07fcb002a77996cf244b4836f69d07a93
Julio Filizzola <juliofilizzola@hotmail.com>
2026-03-28 22:35:20 -0300

    feat: add Rocket class definition
    details:
    - create `Rocket` class with position, velocity, and acceleration attributes
    - implement methods for updating, rendering, and setting position/velocity
    - include `Rocket.hpp` in CMakeLists.txt

---
+++ src/Rocket.hpp
@@ -0,0 +1,17 @@
+#progma once
+#include <SFML/Graphics.hpp>
+
+class Rocket {
+    public:
+    Rocket();
+    void update(sf::Time deltaTime);
+    void setPosition(sf::Vector2f position);
+    void setVelocity(sf::Vector2f velocity);
+    sf::Sprite sprite;
+    void render(sf::RenderWindow &window);
+
+    private:
+    sf::Vector2f myPosition;
+    sf::Vector2f myVelocity{0.f, 0.f};
+    sf::Vector2f myAcceleration{0.f, 0.f};
+};
